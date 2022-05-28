92e63b4431a21fea513ccb76da88f1cd76cd21ed
David Ly <dly5891@gmail.com>
2022-05-28 13:07:44 -0500

    wip

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
+import { ThemeProvider } from "react-bootstrap";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,36 +22,44 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+    >
+      <main>{children}</main>
+    </ThemeProvider>
+  );
+
+  // return (
+  //   <>
+  //     <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+  //     <div
+  //       style={{
+  //         margin: `0 auto`,
+  //         maxWidth: `var(--size-content)`,
+  //         padding: `var(--size-gutter)`,
+  //       }}
+  //     >
+  //       <main>{children}</main>
+  //       <footer
+  //         style={{
+  //           marginTop: `var(--space-5)`,
+  //           fontSize: `var(--font-sm)`,
+  //         }}
+  //       >
+  //         © {new Date().getFullYear()} &middot; Built with
+  //         {` `}
+  //         <a href="https://www.gatsbyjs.com">Gatsby</a>
+  //       </footer>
+  //     </div>
+  //   </>
+  // )
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -94,23 +94,25 @@ const IndexPage = ({ data }) => {
   }));
 
   return (
-    <div>
-      {allPokemon.map(pkmn => {
-        return (
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={pkmn.sprite} />
-            <Card.Body>
-              <Card.Title>{pkmn.name}</Card.Title>
-              {/* <Card.Text>
+    <Layout>
+      <div>
+        {allPokemon.map(pkmn => {
+          return (
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={pkmn.sprite} />
+              <Card.Body>
+                <Card.Title>{pkmn.name}</Card.Title>
+                {/* <Card.Text>
           Some quick example text to build on the card title and make up the bulk of
           the card's content.
-        </Card.Text>
+          </Card.Text>
         <Button variant="primary">Go somewhere</Button> */}
-            </Card.Body>
-          </Card>
-        );
-      })}
-    </div>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </Layout>
   );
 };
 
